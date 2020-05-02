81e6caa248d29c6443d8986eb171ece889261d2f
LKNorman <lknorman30@gmail.com>
2020-05-02 00:11:27 -0400

    added list and nav jsx files

---
+++ src/components/List.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Detail from "./Detail";
+
+const List = (props) => {
+  return (
+    <tbody>
+      {props.employees.map((employee) => (
+        <tr>
+          <Detail {...employee} key={employee.id} />
+        </tr>
+      ))}
+    </tbody>
+  );
+};
+
+export default List;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import logo from "../logo.svg";
+import "../App.css";
+
+const NavBar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <a className="navbar-brand" href="/">
+        <img className="App-logo" src={logo} alt="logo"></img>
+      </a>
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul className="navbar-nav mr-auto">
+          <li className="nav-item">
+            <a className="nav-link" href="/home">
+              Home <span className="sr-only">(current)</span>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/directory">
+              Directory
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
